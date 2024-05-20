b814d0445272283e820bd5ba2bfbad4310647ec6
Kenqz <staticelectricity123@gmail.com>
2024-05-20 14:16:25 +0200

    Solutions to problems

---
+++ leetcode/mergelinkedlists.py
@@ -0,0 +1,39 @@
+class ListNode(object):
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        merged = ListNode()
+
+
+        if not list1:
+            return None
+        if not list2:
+            return None
+
+
+        dummy = ListNode(0)
+        curr = dummy
+        
+        while list1 and list2:
+            if list1.val < list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+
+        curr.next = list1 or list2
+        
+        return dummy.next
+    
+# test case 3 fails, if a value of [0] is provided to link list?
+# fix
